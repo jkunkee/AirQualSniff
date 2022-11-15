30a25c0914fba4c2e003a2d4c831a78bbad64aa6
Jon Kunkee <jonathan.kunkee@gmail.com>
2022-11-15 08:21:10 -0800

    Flesh out Box, add to main code

--- lib/u8g2_boxen/src/boxen.h
+++ lib/u8g2_boxen/src/boxen.h
@@ -9,6 +9,7 @@ public:
 private:
     Box();
     u8g2_t *m_u8g2;
+    BoxValueType m_boxType;
     u8g2_int_t m_x;
     u8g2_int_t m_y;
     u8g2_int_t m_width;
@@ -17,15 +18,15 @@ private:
     char *m_labelTop;
     char *m_labelBottom;
     FontData *m_valueFont;
-    BoxValueType m_boxType;
+    uint8_t m_digits;
     union {
         uint32_t u;
         int32_t i;
         float f;
     } m_value;
-    uint8_t m_digits;
     void Render();
 public:
+    // x,y is the top left corner of the Box, width and height count down and do the right.
     Box(u8g2_t *u8g2, int32_t value,        u8g2_int_t x, u8g2_int_t y, u8g2_int_t width, u8g2_int_t height, const uint8_t *labelFont, char *labelTop, char *labelBottom, const uint8_t *valueFont, uint8_t digits);
     Box(u8g2_t *u8g2, uint32_t value,       u8g2_int_t x, u8g2_int_t y, u8g2_int_t width, u8g2_int_t height, const uint8_t *labelFont, char *labelTop, char *labelBottom, const uint8_t *valueFont, uint8_t digits);
     Box(u8g2_t *u8g2, float value,          u8g2_int_t x, u8g2_int_t y, u8g2_int_t width, u8g2_int_t height, const uint8_t *labelFont, char *labelTop, char *labelBottom, const uint8_t *valueFont, uint8_t digits);
