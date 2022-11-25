e67e596605726bb4dd678a48ea5451902acb2013
Jon Kunkee <jonathan.kunkee@gmail.com>
2022-11-25 00:10:47 -0800

    Simplify Box class

--- lib/u8g2_boxen/src/boxen.h
+++ lib/u8g2_boxen/src/boxen.h
@@ -5,11 +5,10 @@ typedef const uint8_t FontData;
 
 class Box {
 public:
-    enum BoxValueType {BoxTypeFloat, BoxTypeIntSigned, BoxTypeIntUnsigned};
+    enum BoxValueType {BoxTypeUnset, BoxTypeFloat, BoxTypeIntSigned, BoxTypeIntUnsigned};
 private:
     Box();
     u8g2_t *m_u8g2;
-    BoxValueType m_boxType;
     u8g2_int_t m_x;
     u8g2_int_t m_y;
     u8g2_int_t m_width;
@@ -24,15 +23,13 @@ private:
         int32_t i;
         float f;
     } m_value;
-    void Render();
+    void Render(BoxValueType boxType);
 public:
     // x,y is the top left corner of the Box, width and height count down and do the right.
-    Box(u8g2_t *u8g2, int32_t value,        u8g2_int_t x, u8g2_int_t y, u8g2_int_t width, u8g2_int_t height, const uint8_t *labelFont, char *labelTop, char *labelBottom, const uint8_t *valueFont, uint8_t digits);
-    Box(u8g2_t *u8g2, uint32_t value,       u8g2_int_t x, u8g2_int_t y, u8g2_int_t width, u8g2_int_t height, const uint8_t *labelFont, char *labelTop, char *labelBottom, const uint8_t *valueFont, uint8_t digits);
-    Box(u8g2_t *u8g2, float value,          u8g2_int_t x, u8g2_int_t y, u8g2_int_t width, u8g2_int_t height, const uint8_t *labelFont, char *labelTop, char *labelBottom, const uint8_t *valueFont, uint8_t digits);
-    Box(u8g2_t *u8g2, BoxValueType boxType, u8g2_int_t x, u8g2_int_t y, u8g2_int_t width, u8g2_int_t height, const uint8_t *labelFont, char *labelTop, char *labelBottom, const uint8_t *valueFont, uint8_t digits);
+    Box(u8g2_t *u8g2, u8g2_int_t x, u8g2_int_t y, u8g2_int_t width, u8g2_int_t height, const uint8_t *labelFont, char *labelTop, char *labelBottom, const uint8_t *valueFont, uint8_t digits);
 
     void UpdateValue(int32_t val);
     void UpdateValue(uint32_t val);
     void UpdateValue(float val);
+    void UpdateValue();
 };
